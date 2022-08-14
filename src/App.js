bcea0b48ff65295c69415dc025a90ce9615ff54c
Kunal0NQ <kunalninave89@gmail.com>
2022-08-14 07:24:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Cmain from './Components/Cmain.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Cmain />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Register.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function  Register (props)  {
+    return (
+      <div className="container card p-3 my-2 register-container">
+          <h1 className="text-center">Registration Form</h1>
+        <form onSubmit={props.submit}>
+          <div className="form-group">
+            <lable htmlFor="name" >Name:</lable>
+            <input type="text" name="name" className="form-control"></input>
+          </div>
+          <div className="form-group">
+            <lable htmlFor="email" >Email:</lable>
+            <input type="email" name="email" className="form-control"></input>
+          </div>
+          <div className="form-group">
+            <lable htmlFor="password" >Password:</lable>
+            <input type="password" name="password" className="form-control"></input>
+          </div>
+          <button type="submit" className="btn-btn-primary">Register</button>
+        </form>
+      </div>
+    );
+  }
